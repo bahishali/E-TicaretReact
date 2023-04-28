3d2cf361441904a6f6755f9e54b7e5048dfb4258
Büşra Ahıshalı <101969124+bahishali@users.noreply.github.com>
2023-04-28 11:46:08 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import AnaSayfa from "./AnaSayfa"
+import Musteri from "./Musteri"
+import Urun from "./Urun"
+import Siparis from "./Siparis"
+import Login from "./Login2"
+
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AnaSayfa />} />
+          <Route path="/Musteri" element={<Musteri />} />
+          <Route path="/Urun" element={<Urun />} />
+          <Route path="/Siparis" element={<Siparis />} />
+          <Route path="/Login" element={<Login />} />
+
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
+
+
